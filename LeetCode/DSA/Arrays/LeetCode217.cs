28d1189f765f0cd8e7d246914a0586d42870da52
tapendra jaishi <tapendra.jaishi2058@gmail.com>
2025-12-27 17:25:01 +0000

    leet code 217

--- LeetCode/DSA/Arrays/LeetCode217.cs
+++ LeetCode/DSA/Arrays/LeetCode217.cs
@@ -4,5 +4,27 @@ namespace LeetCode
 {
     public class LeetCode217
     {
+        public bool Duplicates(int[] nums)
+        {
+            var hash = new Dictionary<int, int>();
+            for (int i = 0; i < nums.Length; i++)
+            {
+
+
+                if (hash.ContainsKey(nums[i]))
+                {
+                    return true;
+                }
+                else
+                {
+                    hash.Add(nums[i], i);
+                }
+            }
+            return false;
+
+        }
+
+
+
     }
 }
